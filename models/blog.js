266fef0dd9d70f3be90e66e2821f44eca2b7a5a1
vmundra <krishnamundra91@gmail.com>
2021-07-20 00:30:22 +0530

    added backend files

---
+++ models/blog.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const Comment = require('./comment')
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    thumbnail: {
+        type: String
+    },
+    thumbnail_id: {
+        type: String
+    },
+    meta: {
+        type: String,
+        required: true
+    },
+    time_to_read: {
+        type: Number,
+        required: true,
+        validate(value) {
+            if (value < 1) {
+                throw new Error('Must be a postive number')
+            }
+        }
+    },
+    tags: [{
+		type: String,
+		required: true
+        
+    }],
+    genre: [{
+		type: String,
+		required: true
+        
+    }],
+    likes: {
+        type: Number,
+        default: 0,
+    },
+    rating: {
+        type: Number
+        // validate(value) {
+        //     if (value < 0) {
+                
+        //     }
+        // }
+
+    }, 
+	comment_count: {
+		type: Number,
+		default: 0
+	}, 
+	clicks: {
+		type: Number,
+		default: 0
+	}, 
+    content: {
+        type: String,
+        required: true,
+        trim: true
+	},
+	// author: {
+	// type: mongoose.Schema.Types.ObjectId,
+	// required: true,
+	// ref: 'User'
+	// }
+}, {
+    timestamps: true
+})
+
+
+
+// Delete comments when blog is removed And delete cloudinary resources
+blogSchema.pre('remove', async function (next) {
+    const blog = this
+    await Comment.deleteMany({ blog: blog._id })
+    next()
+})
+
+const Blog = mongoose.model('blogs', blogSchema)
+
+module.exports = Blog
---
+++ routes/blog.js
@@ -0,0 +1,301 @@
+const express = require('express')
+const Blog = require('../models/blog')
+const auth = require('../middleware/auth')
+const {algo}= require('./algo')
+
+
+// const blog = Blog.findOne({  }).then((err,res)=>{
+// console.log(res)	
+// })
+// console.log(blog)
+
+const router = new express.Router()
+
+
+
+			const cloudinary = require('cloudinary');
+
+			cloudinary.config({ 
+			  cloud_name: 'dfqdo2aaz', 
+			  api_key: '535848142559685', 
+			  api_secret: 'JNTK18N39FdNA-VvVJ1pCWGjomo' 
+			});
+
+
+
+
+const multer = require("multer");
+
+
+const storage = multer.diskStorage({
+	limits: {
+        fileSize: 10000000
+	},
+	filename: function(req, file, callback) {
+		callback(null, Date.now() + file.originalname); 
+	}
+});
+
+const imageFilter = function (req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif|pdf)$/i)) {
+        return cb(new Error('This file type is not allowed!'), false);
+    }
+    cb(null, true);
+};
+
+const upload = multer({ storage: storage, fileFilter: imageFilter});
+
+
+
+
+
+
+
+
+
+
+router.post('/blog', upload.single('thumbnail'), async (req, res) => {
+    try {
+		let thumbnail,thumbnail_id
+		// if(req.file.qualif){
+		// 		const result = await cloudinary.v2.uploader.upload(req.file.thumbnail.path, {folder:"blog_title"});
+		// 		   thumbnail = result.secure_ul
+		// 		   thumbnail_id = result.public_id
+
+		// }
+		console.log(req.body)
+		const title=req.body.title;
+		const meta = req.body.meta;
+		const time_to_read=req.body.time_to_read;
+		const tags=req.body.tags;
+		const genre=req.body.genre;
+		let rating
+		if(req.body.rating){
+			rating=req.body.rating
+			
+		}
+		const content = req.body.content
+		
+		const blog = new Blog({
+			title,
+			// thumbnail,
+			// thumbnail_id,
+			meta,
+			time_to_read,
+			tags,
+			genre,
+			rating,
+			content
+		});
+        await blog.save()
+        res.status(201).send(blog)
+    } catch (e) {
+		console.log(e)
+        res.status(400).send(e)
+    }
+})
+
+
+
+router.get('/blogs/:id', async (req, res) => {
+    try {
+		const _id = req.params.id;
+        //const blog = await Blog.findOne({ _id })
+		const blog = await Blog.findOneAndUpdate( {_id}, {$inc : {'clicks' : 1}}, {new: true}, {projection:"-clicks"} )
+        res.status(200).send(blog)
+    } 
+	catch (e) {
+		//console.log(e)
+        res.status(400).send(e)
+    }
+})
+
+
+
+// // GET /tag?limit=20&skip=60
+// // GET /tag?sortBy=createdAt:desc
+router.get('/blogs/tags/:tag/:time', async (req, res) => {
+    try {
+		console.log(req.params.time)
+		const options = {}
+		// if (req.query.sortBy) {
+		// 	const parts = req.query.sortBy.split(':')
+		// 	sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+		// }
+		if(req.query.limit){
+			options.limit = parseInt(req.query.limit)
+		}
+		
+		if(req.query.skip){
+			options.skip = parseInt(req.query.skip)			
+		}
+		const tag = req.params.tag;
+		
+        // const blog = await Blog.find({ "tags" : tag }," -content -clicks",options).populate('author','name')
+		const blog = await Blog.find({ "tags" : tag }," -clicks",options).populate('author','name')
+		// console.log(blog)
+		var x;
+		if(req.params.time != 'AllBlogs'){
+			console.log(blog.length)
+			const timerBlog = algo(blog.length,[6,10,1,2,10,6],parseInt(req.params.time))
+			console.log('dsgaadfugiuafdgafodayhyoi',timerBlog)
+			x = timerBlog.filter(function(e){return e});
+			console.log(x)
+		}
+        res.status(200).send(blog)
+    } 
+	catch (e) {
+		//console.log(e)
+        res.status(400).send(e)
+    }
+})
+
+// // GET /tag?limit=20&skip=60
+router.get('/blogs/genres/:genre', async (req, res) => {
+    try {
+		const options = {}
+		if(req.query.limit){
+			options.limit = parseInt(req.query.limit)
+		}
+		
+		if(req.query.skip){
+			options.skip = parseInt(req.query.skip)			
+		}
+		const genre = req.params.genre;
+		
+        const blog = await Blog.find({ "genre" : genre }," -content -clicks",options).populate('author','name')
+        res.status(200).send(blog)
+    } 
+	catch (e) {
+		//console.log(e)
+        res.status(400).send(e)
+    }
+})
+
+
+router.delete('/blogs/:id',async (req,res) => {
+	try {
+        const blog = await Blog.findOneAndDelete({ _id: req.params.id, author: req.user._id })
+
+        if (!blog) {
+            res.status(404).send()
+        }
+
+        res.send(blog)
+    } catch (e) {
+        res.status(500).send()
+    }
+	
+	
+})
+
+// router.patch('/tasks/:id', auth, async (req, res) => {
+//     const updates = Object.keys(req.body)
+//     const allowedUpdates = ['description', 'completed']
+//     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+//     if (!isValidOperation) {
+//         return res.status(400).send({ error: 'Invalid updates!' })
+//     }
+
+//     try {
+//         const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
+
+//         if (!task) {
+//             return res.status(404).send()
+//         }
+
+//         updates.forEach((update) => task[update] = req.body[update])
+//         await task.save()
+//         res.send(task)
+//     } catch (e) {
+//         res.status(400).send(e)
+//     }
+// })
+
+// router.delete('/tasks/:id', auth, async (req, res) => {
+//     try {
+//         const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+
+//         if (!task) {
+//             res.status(404).send()
+//         }
+
+//         res.send(task)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
+
+module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // GET /tasks?completed=true
+// // GET /tasks?limit=10&skip=20
+// // GET /tasks?sortBy=createdAt:desc
+// router.get('/tasks', auth, async (req, res) => {
+//     const match = {}
+//     const sort = {}
+
+//     if (req.query.completed) {
+//         match.completed = req.query.completed === 'true'
+//     }
+
+//     if (req.query.sortBy) {
+//         const parts = req.query.sortBy.split(':')
+//         sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+//     }
+
+//     try {
+//         await req.user.populate({
+//             path: 'tasks',
+//             match,
+//             options: {
+//                 limit: parseInt(req.query.limit),
+//                 skip: parseInt(req.query.skip),
+//                 sort
+//             }
+//         }).execPopulate()
+//         res.send(req.user.tasks)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
+
+// router.get('/tasks/:id', auth, async (req, res) => {
+//     const _id = req.params.id
+
+//     try {
+//         const task = await Task.findOne({ _id, owner: req.user._id })
+
+//         if (!task) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(task)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
